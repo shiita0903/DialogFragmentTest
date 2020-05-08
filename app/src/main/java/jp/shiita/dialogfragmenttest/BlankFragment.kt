648d07c3d6c60f7996d1585b74a0d4647b73dba4
shiita0903 <shiita0903@gmail.com>
2020-05-09 01:32:31 +0900

    Initial commit

---
+++ app/src/main/java/jp/shiita/dialogfragmenttest/BlankFragment.kt
@@ -0,0 +1,24 @@
+package jp.shiita.dialogfragmenttest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_blank.*
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        show_dialog_button.setOnClickListener {
+
+        }
+    }
+}
